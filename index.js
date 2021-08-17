252657418745968668e35070f719a9e1e2f5b262
Juan José Longoria <juan.longoria.lopez@accenture.com>
2021-08-17 11:05:10 +0200

    Created class and methods documentation comments Modify README to addapt to the Aura Helper Framework Readme's style

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const { XML } = require('@ah/languages');
 const XMLParser = XML.XMLParser;
 const XMLUtils = XML.XMLUtils;
-const { Utils, Validator, ProjectUtils } = require('@ah/core').CoreUtils;
+const { Validator, ProjectUtils } = require('@ah/core').CoreUtils;
+const { DataNotFoundException } = require('@ah/core').Exceptions;
 const { MetadataTypes } = require('@ah/core').Values;
-const { MetadataType, MetadataObject, MetadataItem } = require('@ah/core').Types;
+const { MetadataType, MetadataObject, MetadataItem, PackageGeneratorOptions, PackageGeneratorResult } = require('@ah/core').Types;
 const { FileReader, FileWriter, PathUtils } = require('@ah/core').FileSystem;
 const Ignore = require('@ah/ignore');
 const TypesFactory = require('@ah/metadata-factory');
@@ -30,19 +31,68 @@ const NOT_ALLOWED_WILDCARDS = [
 
 ];
 
+/**
+ * Class to create and merge package files to deploy, retrieve or delete metadata from your Salesforce's projects.
+ * 
+ * Can merge several package xml files (including destructiveChanges.xml and destructiveChangesPost.xml files) to combine into one file of each type, 
+ * combine all packages in one file and all detructives in another file or merge all files into one package or destructive file.
+ * 
+ * The file names must contains at least the "package" word to identify the package XML files 
+ * and destructiveChanges or destrutiveChangesPost to the destructive files. 
+ * For example: package1.xml, destructiveChanges_uat.xml, destructiveChangesPost-v34...
+ * 
+ * You can choose a custom API Version to create the packages, 
+ * if not specified API version, the package generator get the higher API version 
+ * from each file types, that is, for package XML files, 
+ * get the higher API of the Package XML files passed, and the same with other file types.
+ */
 class PackageGenerator {
 
+    /**
+     * Method to get the default package generator options. The available options are
+     *      - apiVersion: Api version to create the package. If not provided use the latest api version of the provided files
+     *      - mergePackages: true if want to merge the provided package files
+     *      - mergeDestructives: true if want to merge the provided destructive files into one single file
+     *      - isDestructive: true if you want to merge all files into one destructive file (valid option to merge packages full) 
+     *      - beforeDeploy: true if want to merge destructive files into before deploy destructive file when select mergeDestructives
+     *      - explicit: true if you want to put all metadata types explicit into the file, false to use wildcards when are all checked
+     *      - ignoreFile: path to the ignore file to ignore some metadata types from the packages
+     *      - typesToIgnore: Object with the Metadata Types and Object to ignore, if exists on ignore file and not ignore the rest of the elements of the file
+     * 
+     * @returns Returns a PackageGeneratorOptions object with the default values. The default values are:
+     *      - apiVersion: undefined
+     *      - mergePackages: true
+     *      - mergeDestructives: false
+     *      - isDestructive: false
+     *      - beforeDeploy: false
+     *      - explicit: true
+     *      - ignoreFile: undefined
+     *      - typesToIgnore: undefined
+     */
+    static options() {
+        return new PackageGeneratorOptions();
+    }
+
+    /**
+     * Method to merge several package xml files (including destructiveChanges.xml and destructiveChangesPost.xml files) to combine into one file of each type, combine all packages in one file and all detructives in another file.
+     * @param {String | Array<String>} packageOrDestructiveFiles File or list of files to merge (including package and destructive files in the same list)
+     * @param {String} outputFolder Folder to save the created files
+     * @param {PackageGeneratorOptions} options Package Generator options to choose the options to merge
+     * 
+     * @returns {PackageGeneratorResult} Object with the merge result including the paths of the merged files
+     * 
+     * @throws {WrongDatatypeException} If api version is not a String or number (can be undefined)
+     * @throws {DataNotFoundException} If not package or destructive files provided
+     * @throws {WrongFilePathException} If the package or destructive files path is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the package or destructive files path not exists or not have access to it
+     * @throws {InvalidFilePathException} If the package or destructive files path is not a file
+     * @throws {WrongDirectoryPathException} If the output Folder is not a String or cant convert to absolute path
+     * @throws {DirectoryNotFoundException} If the directory not exists or not have access to it
+     * @throws {InvalidDirectoryPathException} If the path is not a directory
+     */
     static mergePackages(packageOrDestructiveFiles, outputFolder, options) {
         if (!options)
-            options = {
-                apiVersion: undefined,
-                mergePackages: true,
-                mergeDestructives: false,
-                beforeDeploy: false,
-                explicit: false,
-                ignoreFile: undefined,
-                typesForIgnore: undefined,
-            }
+            options = PackageGenerator.options();
         if (options.apiVersion)
             options.apiVersion = ProjectUtils.getApiAsNumber(options.apiVersion);
         if (options.mergePackages === undefined)
@@ -72,14 +122,11 @@ class PackageGenerator {
             }
         }
         if (packages.length === 0 && beforeDestructivePackages.length === 0 && afterDestructivePackages.length === 0)
-            throw new Error('Not package files (' + PACKAGE_NO_EXT + ') or destructive files (' + DESTRUCT_BEFORE_NO_EXT + ', ' + DESTRUCT_AFTER_NO_EXT + ') selected to merge');
+            throw new DataNotFoundException('Not package files (' + PACKAGE_NO_EXT + ') or destructive files (' + DESTRUCT_BEFORE_NO_EXT + ', ' + DESTRUCT_AFTER_NO_EXT + ') selected to merge');
         const mergedPackage = mergePackageFiles(packages, options.apiVersion);
         const mergedBeforeDestructive = mergePackageFiles(beforeDestructivePackages, options.apiVersion);
         const mergedAfterDestructive = mergePackageFiles(afterDestructivePackages, options.apiVersion);
-        const result = {};
-        result[PACKAGE_NO_EXT] = undefined;
-        result[DESTRUCT_BEFORE_NO_EXT] = undefined;
-        result[DESTRUCT_AFTER_NO_EXT] = undefined;
+        const result = new PackageGeneratorResult();
         if (mergedPackage) {
             options.apiVersion = mergedPackage.version;
             result[PACKAGE_NO_EXT] = this.createPackage(TypesFactory.createMetadataTypesFromPackageXML(mergedPackage), outputFolder, options);
@@ -95,16 +142,26 @@ class PackageGenerator {
         return result;
     }
 
+    /**
+     * Method to merge all provided files into only one file. You can choose if merge all into a package.xml, destructiveChanges.xml or destructiveChangesPost.xml
+     * @param {String | Array<String>} packageOrDestructiveFiles file or list of files to merge (including package and destructive files in the same list)
+     * @param {String} outputFolder Folder to save the created files
+     * @param {PackageGeneratorOptions} options Package Generator options to choose the options to merge. If not provided use the default options calling options() method
+     * 
+     * @returns {PackageGeneratorResult} Object with the merge result including the paths of the merged files
+     * 
+     * @throws {WrongDatatypeException} If api version is not a String or number (can be undefined)
+     * @throws {DataNotFoundException} If not package or destructive files provided
+     * @throws {WrongFilePathException} If the package or destructive files path is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the package or destructive files path not exists or not have access to it
+     * @throws {InvalidFilePathException} If the package or destructive files path is not a file
+     * @throws {WrongDirectoryPathException} If the output Folder is not a String or cant convert to absolute path
+     * @throws {DirectoryNotFoundException} If the directory not exists or not have access to it
+     * @throws {InvalidDirectoryPathException} If the path is not a directory
+     */
     static mergePackagesFull(packageOrDestructiveFiles, outputFolder, options) {
         if (!options)
-            options = {
-                apiVersion: undefined,
-                isDestructive: false,
-                beforeDeploy: false,
-                explicit: false,
-                ignoreFile: undefined,
-                typesForIgnore: undefined,
-            }
+            options = PackageGenerator.options();
         if (options.apiVersion)
             options.apiVersion = ProjectUtils.getApiAsNumber(options.apiVersion);
         const packages = [];
@@ -121,12 +178,9 @@ class PackageGenerator {
             }
         }
         if (packages.length === 0)
-            throw new Error('Not package files (' + PACKAGE_NO_EXT + ') or destructive files (' + DESTRUCT_BEFORE_NO_EXT + ', ' + DESTRUCT_AFTER_NO_EXT + ') selected to merge');
+            throw new DataNotFoundException('Not package files (' + PACKAGE_NO_EXT + ') or destructive files (' + DESTRUCT_BEFORE_NO_EXT + ', ' + DESTRUCT_AFTER_NO_EXT + ') selected to merge');
         const mergedPackage = mergePackageFiles(packages, options.apiVersion);
-        const result = {};
-        result[PACKAGE_NO_EXT] = undefined;
-        result[DESTRUCT_BEFORE_NO_EXT] = undefined;
-        result[DESTRUCT_AFTER_NO_EXT] = undefined;
+        const result = new PackageGeneratorResult();
         if (mergedPackage) {
             options.apiVersion = mergedPackage.version;
             if (!options.isDestructive) {
@@ -142,14 +196,25 @@ class PackageGenerator {
         return result;
     }
 
+    /**
+     * Method to get the Package XML format content as String to the selected Metadata JSON file or Metadata JSON Object
+     * @param {String | Object} metadataOrPath Metadata JSON file or Metadata JSON object to get the package or destructive XML content
+     * @param {PackageGeneratorOptions} options Package Generator options to choose the options to merge. If not provided use the default options calling options() method
+     * 
+     * @returns {String} Returns an String with the XML content
+     * 
+     * @throws {WrongDatatypeException} If api version is not a String or number. Can't be empty or undefined
+     * @throws {WrongFilePathException} If the package or destructive file path is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the package or destructive file path not exists or not have access to it
+     * @throws {InvalidFilePathException} If the package or destructive file path is not a file
+     * @throws {WrongFormatException} If file is not a JSON file or not have the correct Metadata JSON format
+     * @throws {WrongDirectoryPathException} If the output Folder is not a String or cant convert to absolute path
+     * @throws {DirectoryNotFoundException} If the directory not exists or not have access to it
+     * @throws {InvalidDirectoryPathException} If the path is not a directory
+     */
     static getPackageContent(metadataOrPath, options) {
         if (!options)
-            options = {
-                apiVersion: undefined,
-                explicit: false,
-                ignoreFile: undefined,
-                typesForIgnore: undefined,
-            }
+            options = PackageGenerator.options();
         let metadata;
         if (typeof metadataOrPath === 'object') {
             metadata = metadataOrPath;
@@ -157,7 +222,7 @@ class PackageGenerator {
             metadata = Validator.validateJSONFile(metadataOrPath);
         }
         if (options.ignoreFile)
-            metadata = Ignore.ignoreMetadata(metadata, options.ignoreFile, options.typesForIgnore);
+            metadata = Ignore.ignoreMetadata(metadata, options.ignoreFile, options.typesToIgnore);
         options.apiVersion = ProjectUtils.getApiAsString(options.apiVersion);
         PackageGenerator.validateJSON(metadata);
         options.explicit = (options.explicit != undefined) ? options.explicit : true;
@@ -174,39 +239,84 @@ class PackageGenerator {
         return packageContent;
     }
 
-    static createPackage(metadataOrPath, folder, options) {
+    /**
+     * Method to create a package XML file with the selected Metadata JSON file or Metadata JSON Object
+     * @param {String | Object} metadataOrPath Metadata JSON file or Metadata JSON object to create the package file
+     * @param {String} outputFolder Folder to save the created file
+     * @param {PackageGeneratorOptions} options Package Generator options to choose the options to merge. If not provided use the default options calling options() method
+     * 
+     * @returns {String} Returns the path to the created file
+     * 
+     * @throws {WrongDatatypeException} If api version is not a String or number. Can't be empty or undefined
+     * @throws {WrongFilePathException} If the package or destructive file path is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the package or destructive file path not exists or not have access to it
+     * @throws {InvalidFilePathException} If the package or destructive file path is not a file
+     * @throws {WrongDirectoryPathException} If the output Folder is not a String or cant convert to absolute path
+     * @throws {DirectoryNotFoundException} If the directory not exists or not have access to it
+     * @throws {InvalidDirectoryPathException} If the path is not a directory
+     * @throws {WrongFormatException} If file is not a JSON file or not have the correct Metadata JSON format
+     */
+    static createPackage(metadataOrPath, outputFolder, options) {
         if (!options)
-            options = {
-                apiVersion: undefined,
-                explicit: false,
-                ignoreFile: undefined,
-                typesForIgnore: undefined,
-            }
-        return createPackageFile(folder, PACKAGE_FILENAME, PackageGenerator.getPackageContent(metadataOrPath, options));
+            options = PackageGenerator.options();
+        return createPackageFile(outputFolder, PACKAGE_FILENAME, PackageGenerator.getPackageContent(metadataOrPath, options));
     }
 
-    static createBeforeDeployDestructive(metadataOrPath, folder, options) {
+    /**
+     * Method to create a before deploy destructive file with the selected Metadata JSON file or Metadata JSON Object
+     * @param {String | Object} metadataOrPath Metadata JSON file or Metadata JSON object to create the destructive file
+     * @param {String} outputFolder Folder to save the created file
+     * @param {PackageGeneratorOptions} options Package Generator options to choose the options to merge. If not provided use the default options calling options() method
+     * 
+     * @returns {String} Returns the path to the created file
+     * 
+     * @throws {WrongDatatypeException} If api version is not a String or number. Can't be empty or undefined
+     * @throws {WrongFilePathException} If the package or destructive file path is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the package or destructive file path not exists or not have access to it
+     * @throws {InvalidFilePathException} If the package or destructive file path is not a file
+     * @throws {WrongFormatException} If file is not a JSON file or not have the correct Metadata JSON format
+     * @throws {WrongDirectoryPathException} If the output Folder is not a String or cant convert to absolute path
+     * @throws {DirectoryNotFoundException} If the directory not exists or not have access to it
+     * @throws {InvalidDirectoryPathException} If the path is not a directory
+     */
+    static createBeforeDeployDestructive(metadataOrPath, outputFolder, options) {
         if (!options)
-            options = {
-                apiVersion: undefined,
-                explicit: false,
-                ignoreFile: undefined,
-                typesForIgnore: undefined,
-            }
-        return createPackageFile(folder, DESTRUCT_BEFORE_FILENAME, PackageGenerator.getPackageContent(metadataOrPath, options));
+            options = PackageGenerator.options();
+        return createPackageFile(outputFolder, DESTRUCT_BEFORE_FILENAME, PackageGenerator.getPackageContent(metadataOrPath, options));
     }
 
-    static createAfterDeployDestructive(metadataOrPath, folder, options) {
+    /**
+     * Method to create an after deploy destructive file with the selected Metadata JSON file or Metadata JSON Object
+     * @param {String | Object} metadataOrPath Metadata JSON file or Metadata JSON object to create the destructive file
+     * @param {String} outputFolder Folder to save the created file
+     * @param {PackageGeneratorOptions} options Package Generator options to choose the options to merge. If not provided use the default options calling options() method
+     * 
+     * @returns {String} Returns the path to the created file
+     * 
+     * @throws {WrongDatatypeException} If api version is not a String or number. Can't be empty or undefined
+     * @throws {WrongFilePathException} If the package or destructive file path is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the package or destructive file path not exists or not have access to it
+     * @throws {InvalidFilePathException} If the package or destructive file path is not a file
+     * @throws {WrongFormatException} If file is not a JSON file or not have the correct Metadata JSON format
+     * @throws {WrongDirectoryPathException} If the output Folder is not a String or cant convert to absolute path
+     * @throws {DirectoryNotFoundException} If the directory not exists or not have access to it
+     * @throws {InvalidDirectoryPathException} If the path is not a directory
+     */
+    static createAfterDeployDestructive(metadataOrPath, outputFolder, options) {
         if (!options)
-            options = {
-                apiVersion: undefined,
-                explicit: false,
-                ignoreFile: undefined,
-                typesForIgnore: undefined,
-            }
-        return createPackageFile(folder, DESTRUCT_AFTER_FILENAME, PackageGenerator.getPackageContent(metadataOrPath, options));
+            options = PackageGenerator.options();
+        return createPackageFile(outputFolder, DESTRUCT_AFTER_FILENAME, PackageGenerator.getPackageContent(metadataOrPath, options));
     }
 
+    /**
+     * Method to validate a Metadata JSON file or Metadata JSON Object format. If is not valid, throw several exceptions.
+     * @param {String | Object} metadataOrPath Metadata JSON file or Metadata JSON object to validate
+     * 
+     * @throws {WrongFilePathException} If the filePath is not a String or cant convert to absolute path
+     * @throws {FileNotFoundException} If the file not exists or not have access to it
+     * @throws {InvalidFilePathException} If the path is not a file
+     * @throws {WrongFormatException} If file is not a JSON file or not have the correct Metadata JSON format
+     */
     static validateJSON(metadataOrPath) {
         Validator.validateMetadataJSON(metadataOrPath);
     }
